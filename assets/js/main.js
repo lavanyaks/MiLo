1b8f2ab091f5d14601d739f42dab7f896bf1d0a7
lavanyaks <lavanya.singh24@gmail.com>
2019-06-08 15:43:20 -0400

    Adding header, footer and basic styling

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+var showMenu = document.getElementsByClassName('menu-icon');
+var closeMenu = document.getElementsByClassName('menu-close');
+var navMenu = document.getElementsByClassName('slide-header');
+var mainClass = document.getElementsByClassName('page-content');
+var menuOption = document.getElementsByClassName('menu-option');
+var subnav = document.getElementsByClassName('subnav');
+
+showMenu[0].addEventListener('click', function () {
+    if (navMenu[0].classList.contains('open')) {
+        navMenu[0].classList.remove('open');
+        mainClass[0].classList.remove('open');
+    }
+    else {
+        navMenu[0].className += " open";
+        mainClass[0].classList += " open";
+    }
+});
+
+mainClass[0].addEventListener('click', function (event) {
+    if (navMenu[0].classList.contains('open')) {
+        navMenu[0].classList.remove('open');
+        mainClass[0].classList.remove('open');
+    }
+});
+
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./service-worker.js").then(function() {
+        console.log("Service Worker Registered");
+    });
+}
